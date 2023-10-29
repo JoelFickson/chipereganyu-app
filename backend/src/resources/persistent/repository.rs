7085ea1f2d278551542a993472744a3018f6f943
Joel Fickson <Joelngozo@gmail.com>
2023-10-29 13:04:02 +0100

    chore: add seaorm

--- backend/src/resources/persistent/repository.rs
+++ backend/src/resources/persistent/repository.rs
@@ -1,9 +1,19 @@
-use axum::Json;
+use sea_orm::entity::prelude::*;
+use sea_orm::Insert;
 
+async fn create<T, DB>(data: T, db: &DB) -> Result<T, E>
+    where
+        DB: sea_orm::DatabaseConnection,
+        T: Entity,
+{
+    let mut model = data.into_active_model();
+    let id = db
+        .insert(model)
+        .await?
+        .last_insert_id()
+        .ok_or(E::DatabaseError)?;
 
+    model.set_id(id);
 
-async fn create<T>(data: T)-> Result<T, E>{
-    todo!()
+    Ok(model.into())
 }
-
-async fn get_all()->
